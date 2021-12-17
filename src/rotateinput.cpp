f6e953311681aa905c9ee42e9fbab306e382330f
JensZorn <90791407+JensZorn@users.noreply.github.com>
2021-12-17 23:04:34 +0100

    Update rotateinput.cpp

--- src/rotateinput.cpp
+++ src/rotateinput.cpp
@@ -167,13 +167,23 @@ void RotateInput::rotate(Orientation orientation)
     {LeftUp, {0, -1, 1, 1, 0, 0, 0, 0, 1}},
     {RightUp, {0, 1, 0, -1, 0, 1, 0, 0, 1}},
   };
+  static QHash<Orientation, vector<float>>  orientation_matrixtouchpad_map {
+    {TopUp, {1, 0, 0, 0, 1, 0, 0, 0, 1}},
+    {TopDown, {-1, 0, 1, 0, -1, 1, 0, 0, 1}},
+    {LeftUp, {0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    {RightUp, {0, 1, 0, -1, 0, 1, 0, 0, 1}},
   auto orientation_matrix = orientation_matrix_map[orientation];
+  auto orientation_matrixtouchpad = orientation_matrixtouchpad_map[orientation];
   qDebug() << "Setting rotation matrix: " << orientation_matrix;
   for(auto device: d->devices) {
     for(auto property: device.properties()) {
       if(property.isRotationMatrix()) {
         qDebug() << "Changing orientation matrix for device " << device.name << ", property " << property.name;
-        property.setRotationMatrix(orientation_matrix);
+        if(device.name=="Asus TouchPad"){
+          property.setRotationMatrix(orientation_matrixtouchpad);
+        } else {
+          property.setRotationMatrix(orientation_matrix);
+        }
       }
     }
   }
